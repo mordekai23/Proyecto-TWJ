b8b208d370fdb72be8a96dd3373ad3980bc75f74
Evelyn Regalado <evelyn.regalado@epn.edu.ec>
2017-08-06 11:11:54 -0500

    Api-Usuario

---
+++ Sails/Cine/api/models/Usuario.js
@@ -0,0 +1,24 @@
+/**
+ * Usuario.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre:{
+      type:'string'
+    },
+    correo:{
+      type:'email'
+    },
+    password:{
+      type:'string',
+      required:true
+    }
+
+  }
+};
+
