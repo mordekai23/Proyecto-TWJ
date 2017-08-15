ccd3199197036ad4564bc91bcc58003ecd2f9ca7
Evelyn Regalado <evelyn.regalado@epn.edu.ec>
2017-08-15 15:56:58 -0500

    HAsh!!!!

--- Sails/Cine/api/models/Usuario.js
+++ Sails/Cine/api/models/Usuario.js
@@ -4,7 +4,7 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
-
+var Passwords = require('machinepack-passwords');
 module.exports = {
   connection: 'localDiskDb',
   attributes: {
@@ -17,8 +17,41 @@ module.exports = {
 
     },
     password:{
-      type:'string'
+      type:'string',
+      required: true
 
+    }
+  },
+  beforeCreate: function (usuario, cb) {
+    sails.log.info('Usuario', usuario);
+    Passwords.encryptPassword({
+      password: usuario.password
+    }).exec({
+      error: function (err) {
+        cb('Error de enciptacion', err);
+      },
+      success: function (hashedPassword) {
+        usuario.password = hashedPassword;
+        cb();
+      }
+    });
+  },
+  beforeUpdate: function (valoresAActualizar, cb) {
+    sails.log.info('Usuario', valoresAActualizar);
+    if(valoresAActualizar.password) {
+      Passwords.encryptPassword({
+        password: valoresAActualizar.password
+      }).exec({
+        error: function (err) {
+          cb('Error de enciptacion', err);
+        },
+        success: function (hashedPassword) {
+          valoresAActualizar.password = hashedPassword;
+          cb();
+        }
+      });
+    } else {
+      cb();
     }
   }
 };
